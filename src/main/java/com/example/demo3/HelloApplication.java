7d650cc469823af9e6809a6233246f02eb7f6191
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2022-11-18 13:01:59 -0500

    calculator-window

--- src/main/java/com/example/demo3/HelloApplication.java
+++ src/main/java/com/example/demo3/HelloApplication.java
@@ -1,109 +1,167 @@
+// Name: Jayden Nguyen, ID: 101363755, Lab Test
+
+
 package com.example.demo3;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
+import java.util.*;
+
+// a simple JavaFX calculator.
 public class HelloApplication extends Application {
+    private static final String[][] template = {
+            {"7", "8", "9", "/"},
+            {"4", "5", "6", "*"},
+            {"1", "2", "3", "-"},
+            {"0", "c", "=", "+"}
+    };
+
+    private final Map<String, Button> accelerators = new HashMap<>();
+
+    private final DoubleProperty stackValue = new SimpleDoubleProperty();
+    private final DoubleProperty value = new SimpleDoubleProperty();
+
+    private enum Op {NOOP, ADD, SUBTRACT, MULTIPLY, DIVIDE}
+
+    private Op curOp = Op.NOOP;
+    private Op stackOp = Op.NOOP;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Tip Calculator");
+    public void start(Stage stage) {
+        final TextField screen = createScreen();
+        final TilePane buttons = createButtons();
 
-        // Create a GridPane layout
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        stage.setTitle("Calc");
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setResizable(false);
+        stage.setScene(new Scene(createLayout(screen, buttons)));
+        stage.show();
+    }
 
-        // Add Text, TextFields and Labels to the grid
-        Text sceneTitle = new Text("Welcome to the Tip Calculator");
-        grid.add(sceneTitle, 0, 0, 2, 1);
+    private VBox createLayout(TextField screen, TilePane buttons) {
+        final VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.setStyle("-fx-background-color: chocolate; -fx-padding: 20; -fx-font-size: 20;");
+        layout.getChildren().setAll(screen, buttons);
+        handleAccelerators(layout);
+        screen.prefWidthProperty().bind(buttons.widthProperty());
+        return layout;
 
-        Label tableTotalLabel = new Label("Table Total:");
-        grid.add(tableTotalLabel, 0, 1);
+    }
 
-        TextField tableTotalTextField = new TextField();
-        grid.add(tableTotalTextField, 1, 1);
-
-        Label numberOfGuestsLabel = new Label("Number of Guests:");
-        grid.add(numberOfGuestsLabel, 0, 2);
-
-        TextField numberOfGuestsTextField = new TextField();
-        grid.add(numberOfGuestsTextField, 1, 2);
-
-        Button calculateButton = new Button("Calculate");
-        HBox hBox = new HBox(10);
-        hBox.setAlignment(Pos.BOTTOM_RIGHT);
-        hBox.getChildren().add(calculateButton);
-        grid.add(hBox, 1, 4);
-
-        // Create a Label for displaying the calculated gratuity
-        Label gratuityLabel = new Label("Gratuity:");
-        grid.add(gratuityLabel, 0, 6);
-
-        Text gratuityText = new Text();
-        grid.add(gratuityText, 1, 6);
-
-        // Calculate button action
-        calculateButton.setOnAction(e -> {
-            // Get the table total from the TextField
-            String tableTotalString = tableTotalTextField.getText();
-
-            // Get the number of guests from the TextField
-            String numberOfGuestsString = numberOfGuestsTextField.getText();
-
-            // Perform input validation
-            if (tableTotalString == null || tableTotalString.trim().isEmpty()) {
-                // Show an error message
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Invalid input");
-                alert.setContentText("Please enter a valid table total.");
-
-                alert.showAndWait();
-            } else if (numberOfGuestsString == null || numberOfGuestsString.trim().isEmpty()) {
-                // Show an error message
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Invalid input");
-                alert.setContentText("Please enter a valid number of guests.");
-
-                alert.showAndWait();
-            } else {
-                // Calculate the gratuity
-                double tableTotal = Double.parseDouble(tableTotalString);
-                int numberOfGuests = Integer.parseInt(numberOfGuestsString);
-
-                double gratuity = 0.0;
-
-                if (numberOfGuests < 8) {
-                    gratuity = tableTotal * 0.1;
-                } else if (numberOfGuests >= 8 && numberOfGuests <= 15) {
-                    gratuity = tableTotal * 0.15;
-                } else if (numberOfGuests > 15) {
-                    gratuity = tableTotal * 0.2;
-                }
-
-                // Display the gratuity
-                gratuityText.setText(Double.toString(gratuity));
+    private void handleAccelerators(VBox layout) {
+        layout.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+            Button activated = accelerators.get(keyEvent.getText());
+            if (activated != null) {
+                activated.fire();
             }
         });
+    }
 
-        // Set the scene and show the stage
-        Scene scene = new Scene(grid, 400, 275);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    private TextField createScreen() {
+        final TextField screen = new TextField();
+        screen.setStyle("-fx-background-color: aquamarine;");
+        screen.setAlignment(Pos.CENTER_RIGHT);
+        screen.setEditable(false);
+        screen.textProperty().bind(Bindings.format("%.0f", value));
+        return screen;
+    }
+
+    private TilePane createButtons() {
+        TilePane buttons = new TilePane();
+        buttons.setVgap(7);
+        buttons.setHgap(7);
+        buttons.setPrefColumns(template[0].length);
+        for (String[] r : template) {
+            for (String s : r) {
+                buttons.getChildren().add(createButton(s));
+            }
+        }
+        return buttons;
+    }
+
+    private Button createButton(final String s) {
+        Button button = makeStandardButton(s);
+
+        if (s.matches("[0-9]")) {
+            makeNumericButton(s, button);
+        } else {
+            final ObjectProperty<Op> triggerOp = determineOperand(s);
+            if (triggerOp.get() != Op.NOOP) {
+                makeOperandButton(button, triggerOp);
+            } else if ("c".equals(s)) {
+                makeClearButton(button);
+            } else if ("=".equals(s)) {
+                makeEqualsButton(button);
+            }
+        }
+
+        return button;
+    }
+
+    private ObjectProperty<Op> determineOperand(String s) {
+        final ObjectProperty<Op> triggerOp = new SimpleObjectProperty<>(Op.NOOP);
+        switch (s) {
+            case "+" -> triggerOp.set(Op.ADD);
+            case "-" -> triggerOp.set(Op.SUBTRACT);
+            case "*" -> triggerOp.set(Op.MULTIPLY);
+            case "/" -> triggerOp.set(Op.DIVIDE);
+        }
+        return triggerOp;
+    }
+
+    private void makeOperandButton(Button button, final ObjectProperty<Op> triggerOp) {
+        button.setStyle("-fx-base: lightgray;");
+        button.setOnAction(actionEvent -> curOp = triggerOp.get());
+    }
+
+    private Button makeStandardButton(String s) {
+        Button button = new Button(s);
+        button.setStyle("-fx-base: beige;");
+        accelerators.put(s, button);
+        button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        return button;
+    }
+
+    private void makeNumericButton(final String s, Button button) {
+        button.setOnAction(actionEvent -> {
+            if (curOp == Op.NOOP) {
+                value.set(value.get() * 10 + Integer.parseInt(s));
+            } else {
+                stackValue.set(value.get());
+                value.set(Integer.parseInt(s));
+                stackOp = curOp;
+                curOp = Op.NOOP;
+            }
+        });
+    }
+
+    private void makeClearButton(Button button) {
+        button.setStyle("-fx-base: mistyrose;");
+        button.setOnAction(actionEvent -> value.set(0));
+    }
+
+    private void makeEqualsButton(Button button) {
+        button.setStyle("-fx-base: ghostwhite;");
+        button.setOnAction(actionEvent -> {
+            switch (stackOp) {
+                case ADD -> value.set(stackValue.get() + value.get());
+                case SUBTRACT -> value.set(stackValue.get() - value.get());
+                case MULTIPLY -> value.set(stackValue.get() * value.get());
+                case DIVIDE -> value.set(stackValue.get() / value.get());
+            }
+        });
     }
 }
-
